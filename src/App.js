77f812130c427a892daac428d018111d7a08390b
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-09-20 16:47:55 -0300

    Implementação da função IniciaJogo

--- src/App.js
+++ src/App.js
@@ -7,38 +7,64 @@ function comparador() {
 }
 
 
+
 export default function App(){
     const alfabeto = ["A","B","C","D","E","G","G","H","I","J","K","L","M",
                     "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     let arrayPalavra = [];
-    const [iniciouJogo, mudaIniciouJogo] = useState(true);
-    function IniciaJogo(){
+    const [iniciouJogo, mudaIniciouJogo] = useState(false);
+    const [habilitaTeclas, mudaHabilitaTeclas] = useState(false);
+
+    function RenderizaTeclas(props) {
+        const [teclasCor, mudaTeclasCor] = useState(props.estado);
+        return (
+              <li className={teclasCor} onClick={() => mudaTeclasCor("desabilitado")}>{props.letra}</li>
+        );
+      }
+
+    function IniciaJogo() {
         const palavra = palavras.sort(comparador);
         arrayPalavra = palavra[0].split("");
-        console.log(arrayPalavra)
         return (
-            <ul>
-                {arrayPalavra.map((letra) => <li>{letra}</li>)}
-            </ul>
-            )
+        <ul>
+            {arrayPalavra.map(() => (
+            <li>_</li>
+            ))}
+        </ul>
+        );
     }
-    return(
-        <main>
-            <section className="jogo">
-                <img src="./assets/forca0.png"/>
-                <button onClick={mudaIniciouJogo(true)}>Escolher Palavra</button>
-                {iniciouJogo ? IniciaJogo : <ul></ul>}
-            </section>
-            <section className="teclas">
-                <ul>
-                    {alfabeto.map((letra) => <li>{letra}</li>)}
-                </ul>
-            </section>
-            <section className="chute">
-                Já sei a palavra!
-                <input></input>
-                <button>Chutar</button>
-            </section>
-        </main>
-    )
+    return (
+      <main>
+        <section className="jogo">
+          <img src="./assets/forca0.png" />
+
+          <button
+            onClick={() => {
+              mudaIniciouJogo(true);
+              mudaHabilitaTeclas(true);
+            }}
+          >
+            Escolher Palavra
+          </button>
+          {iniciouJogo ? IniciaJogo() : <ul></ul>}
+        </section>
+        <section className="teclas">
+          <ul>
+            {habilitaTeclas ?  
+                alfabeto.map((letra) => (
+              <RenderizaTeclas letra={letra} estado="habilitado" />
+            )) : alfabeto.map((letra) => (
+                <RenderizaTeclas letra={letra} estado="desabilitado" />
+              ))
+            }
+            
+          </ul>
+        </section>
+        <section className="chute">
+          Já sei a palavra!
+          <input></input>
+          <button>Chutar</button>
+        </section>
+      </main>
+    );
 }
