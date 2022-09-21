b4d2d1d87e796dab72869d1684f8821fb56d82a3
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-09-20 22:00:53 -0300

    Funções que verificam a letra clicada

--- src/App.js
+++ src/App.js
@@ -1,62 +1,97 @@
 import palavras from "./palavras"
 import { useState } from "react";
+import imgInicial from "./assets/forca0.png"
+
+let arrayPalavra = [];
+let arrayEstadoJogo = [];
 
 // função que gera números aleatórios no intervalo [-0.5,0.5]
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-
+function sorteiaPalavra(){
+  const palavra = palavras.sort(comparador);
+  return palavra[0].split("");
+}
 
 export default function App(){
-    const alfabeto = ["A","B","C","D","E","G","G","H","I","J","K","L","M",
+    const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
                     "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    let arrayPalavra = [];
-    const [iniciouJogo, mudaIniciouJogo] = useState(false);
+    let contadorCliques = 0;
+    const [faseDoJogo, mudaFaseDoJogo] = useState("");
     const [habilitaTeclas, mudaHabilitaTeclas] = useState(false);
-    const [habilitaInput, mudaHabilitaInput] = useState(true)
+    const [habilitaInput, mudaHabilitaInput] = useState(true);
+    const [teclaClicada, mudaTeclaClicada] = useState("");
 
     function RenderizaTeclas(props) {
         const [teclasCor, mudaTeclasCor] = useState(props.estado);
+        function verificaTecla(){
+            if(teclasCor === "habilitado"){
+                mudaTeclasCor("desabilitado");
+                contadorCliques++;
+                mudaTeclaClicada(props.letra.toLowerCase());
+                mudaFaseDoJogo("andamento");
+            }
+        }
+
         return (
-              <li className={teclasCor} onClick={() => mudaTeclasCor("desabilitado")}>{props.letra}</li>
+              <li key={props.index} className={teclasCor} onClick={verificaTecla}>{props.letra}</li>
         );
       }
 
-    function IniciaJogo() {
-        const palavra = palavras.sort(comparador);
-        arrayPalavra = palavra[0].split("");
-        return (
-        <ul>
-            {arrayPalavra.map(() => (
-            <li>_</li>
-            ))}
-        </ul>
-        );
+    function AtualizaJogo(props) {
+        if (faseDoJogo === "inicio") {      
+            return (
+              <ul>
+                {arrayPalavra.map((letra,index) => (
+                  <li key={index}>_</li>
+                ))}
+              </ul>
+            );
+        }
+          else if (faseDoJogo === "andamento") { 
+            console.log(props.letraTecla);  
+            console.log(arrayPalavra); 
+            return (
+              <ul>
+                {arrayPalavra.map((letra,index) => {
+                    if(letra === props.letraTecla){
+                      arrayEstadoJogo[index] = letra            
+                    }
+                    return(<li key={index}>{arrayEstadoJogo[index]}</li>)
+                }                
+                )}
+              </ul>
+            );
+          }       
     }
     return (
       <main>
         <section className="jogo">
-          <img src="./assets/forca0.png" />
+          <img src={imgInicial} />
 
           <button
             onClick={() => {
-              mudaIniciouJogo(true);
+              arrayPalavra = sorteiaPalavra()
+              mudaFaseDoJogo("inicio");
+              arrayPalavra.forEach(() => arrayEstadoJogo.push("_"));
+              console.log(arrayEstadoJogo);
               mudaHabilitaTeclas(true);
               mudaHabilitaInput(false);
             }}
           >
             Escolher Palavra
           </button>
-          {iniciouJogo ? IniciaJogo() : <ul></ul>}
+          {(faseDoJogo !== "") ? <AtualizaJogo letraTecla={teclaClicada}/> : <ul></ul>}
         </section>
         <section className="teclas">
           <ul>
             {habilitaTeclas ?  
-                alfabeto.map((letra) => (
-              <RenderizaTeclas letra={letra} estado="habilitado" />
-            )) : alfabeto.map((letra) => (
-                <RenderizaTeclas letra={letra} estado="desabilitado" />
+                alfabeto.map((letra,index) => (
+              <RenderizaTeclas letra={letra} estado="habilitado" index={index} />
+            )) : alfabeto.map((letra,index) => (
+                <RenderizaTeclas letra={letra} estado="desabilitado" index={index} />
               ))
             }
             
